70dff11cc7c6303fd4c7002ce8731c248ecbcef9
Szymon Rymarowicz <50682083+szymon-rymarowicz@users.noreply.github.com>
2020-01-09 09:53:04 +0100

    landing-page

---
+++ js/app.js
@@ -0,0 +1,25 @@
+
+// onclick="menu_open()"
+// onclick="menu_close()"
+
+// const media = window.matchMedia("(min-width: 769px)");
+//
+// if (media.matches) {
+//     document.querySelector(".cross").style.display = "none";
+//     document.querySelector(".menu_hamburger").style.display = "none";
+//     document.querySelector("nav").style.display = "block"
+// }
+//
+// else {
+//     function menu_open() {
+//         document.querySelector(".menu_hamburger").style.display = "none";
+//         document.querySelector(".cross").style.display = "block";
+//         document.querySelector("nav").style.display = "block";
+//     }
+//
+//     function menu_close() {
+//         document.querySelector(".menu_hamburger").style.display = "block";
+//         document.querySelector(".cross").style.display = "none";
+//         document.querySelector("nav").style.display = "none";
+//     }
+// }
