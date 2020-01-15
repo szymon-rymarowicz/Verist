6c9cd16ef923b5ec2d2b4476fa04bd824d53fd90
Szymon Rymarowicz <50682083+szymon-rymarowicz@users.noreply.github.com>
2020-01-15 13:07:50 +0100

    update

--- js/app.js
+++ js/app.js
@@ -23,3 +23,54 @@
 //         document.querySelector("nav").style.display = "none";
 //     }
 // }
+
+
+// function scrollTo(element) {
+//     window.scroll({
+//         behavior: 'smooth',
+//         left: 0,
+//         top: element.offsetTop + 350
+//     });
+// }
+//
+//     let navigationLinks = document.querySelectorAll('.top-bar .navigation__link');
+//
+//
+//
+// navigationLinks.forEach(link => link.addEventListener('click', function (e) {
+//
+//     var section = document.querySelector(`${e.target.hash}`);
+//
+//     console.log(section);
+//
+//     scrollTo(section)
+//
+// }));
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+
+
+const scrollFunction = () => {
+
+    let link = document.querySelectorAll(".navigation__link");
+    let topBar = document.querySelector(".top-bar");
+    if (window.pageYOffset > topBar.offsetTop) {
+        topBar.classList.add("top-bar--navigation-scroll");
+        for (let i = 0; i < link.length; i++ ) {
+            link[i].classList.add("paragraph--white");
+        }
+
+    } else {
+        topBar.classList.remove("top-bar--navigation-scroll");
+        for (let i = 0; i < link.length; i++ ) {
+            link[i].classList.remove("paragraph--white");
+        }
+    }
+}
+
+
+    window.addEventListener('scroll', scrollFunction);
+});
+
